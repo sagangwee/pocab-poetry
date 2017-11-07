4ec789a110d2617104b6b15e89897430c8dcc631
Sagang Wee <sagangwee@yahoo.com>
2017-11-06 22:25:35 -0800

    added static url

--- pocabpoetry/settings.py
+++ pocabpoetry/settings.py
@@ -117,9 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 PROJECT_DIR = os.path.join(PROJECT_ROOT,'poetry')
+STATIC_URL = os.path.join(PROJECT_DIR,'static/')
 STATIC_ROOT= os.path.join(PROJECT_DIR,'staticfiles/')
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT,'static/'),
