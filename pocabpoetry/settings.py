0d0decf0ef5d42c2375ec1a38c2837fe79268de0
Sagang Wee <sagangwee@yahoo.com>
2017-11-06 22:20:33 -0800

    added os paths to settings

--- pocabpoetry/settings.py
+++ pocabpoetry/settings.py
@@ -118,4 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static'
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+STATIC_ROOT= os.path.join(PROJECT_DIR,'staticfiles/')
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT,'static/'),
+)
